616b8dee3df26cd7192b555b29360dd96d51e022
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-11-21 19:20:14 +0300

    tasarımsal calismalar

---
+++ OnlineEgitimClient/Controllers/PurchasedCourseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEgitimClient.Controllers
+{
+    public class PurchasedCourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CourseContent()
+        {
+            return View();
+        }
+    }
+}
